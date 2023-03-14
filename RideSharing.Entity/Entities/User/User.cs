b418807caf922ad42e0be80be84435bfdd26c75f
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-03-14 17:09:31 +0600

    customer driver inherited from microsoft IdentityUser

--- RideSharing.Entity/Entities/User/User.cs
+++ RideSharing.Entity/Entities/User/User.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using RideSharing.Entity.Enums;
 
 namespace RideSharing.Entity
 {
-    public class User : Base
+    public class User : IdentityUser<long>
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public long Phone { get; set; }
+        public User()
+        {
+            this.CreatedDateUtc = DateTime.UtcNow;
+            this.Gender = Gender.Unknown;
+        }
+
+        public Gender Gender { get; set; }
+
+        // props I couldn't inherit from Base due to multiple inheritance doesn't support!
+        public DateTime CreatedDateUtc { get; set; }
+        public DateTime? UpdatedDateUtc { get; set; }
+        public DateTime? DeletedDateUtc { get; set; }
+
     }
 }
