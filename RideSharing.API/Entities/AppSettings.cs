c7d3dfa42623eba81829faa4d6984f1eccfcdae1
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-04-05 00:45:55 +0600

    folder restructured

--- RideSharing.API/Entities/AppSettings.cs
+++ RideSharing.API/Entities/AppSettings.cs
@@ -7,8 +7,8 @@ namespace RideSharing.API
 {
     public class AppSettings
     {
-        public string ConnectionString {  get; set; }
-        public string JwtSecretKey {  get; set; }
-        public string ClientUrl {  get; set; }
+        public string ConnectionString { get; set; }
+        public string JwtSecretKey { get; set; }
+        public string ClientUrl { get; set; }
     }
 }
