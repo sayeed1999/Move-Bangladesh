22456fe1cca284065de9423324e8a8cafcb46e7a
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-05-07 16:31:57 +0600

    test written (not working)

--- RideSharing.Tests/CustomerServiceTest.cs
+++ RideSharing.Tests/CustomerServiceTest.cs
@@ -7,22 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
 
 namespace RideSharing.Tests
 {
     public class CustomerServiceTest
     {
-        private ICustomerService customerService;
+        private readonly Mock<IBaseRepository<Customer>> _mockRepository;
+        private readonly IServiceProvider _serviceProvider;
 
         public CustomerServiceTest()
         {
+            _mockRepository = new Mock<IBaseRepository<Customer>>();
 
+            var services = new ServiceCollection();
+            services.AddTransient<IBaseRepository<Customer>>(sp => _mockRepository.Object);
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private IBaseRepository<Customer> GetCustomerService()
+        {
+            var service = _serviceProvider.GetRequiredService<IBaseRepository<Customer>>();
+            return service;
         }
 
         [Fact]
-        public async void GetCustomers()
+        public async Task GetCustomers()
         {
+            // Arrange
+            var customerService = GetCustomerService();
+            // Add any necessary setup for your mock repository
 
+            // Act
+            var customers = await customerService.GetAllAsync();
+        
+            // Assert
+            Assert.Equal(0, customers.Count());
         }
+
     }
 }
