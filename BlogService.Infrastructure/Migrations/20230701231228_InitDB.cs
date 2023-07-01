67bb98e637027219ed42d54eec71377a3df2d574
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-07-02 05:13:33 +0600

    deleted and remigrated database

---
+++ BlogService.Infrastructure/Migrations/20230701231228_InitDB.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nodes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NodeType = table.Column<int>(type: "int", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedById = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedDateUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedById = table.Column<long>(type: "bigint", nullable: true),
+                    UpdatedDateUtc = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedById = table.Column<long>(type: "bigint", nullable: true),
+                    DeletedDateUtc = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Nodes_User_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Nodes_User_DeletedById",
+                        column: x => x.DeletedById,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Nodes_User_UpdatedById",
+                        column: x => x.UpdatedById,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Edges",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EdgeType = table.Column<int>(type: "int", nullable: false),
+                    FromDestinationId = table.Column<long>(type: "bigint", nullable: false),
+                    ToDestinationId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedById = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedDateUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedById = table.Column<long>(type: "bigint", nullable: true),
+                    UpdatedDateUtc = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedById = table.Column<long>(type: "bigint", nullable: true),
+                    DeletedDateUtc = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Edges", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Edges_Nodes_FromDestinationId",
+                        column: x => x.FromDestinationId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Edges_Nodes_ToDestinationId",
+                        column: x => x.ToDestinationId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Edges_User_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Edges_User_DeletedById",
+                        column: x => x.DeletedById,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Edges_User_UpdatedById",
+                        column: x => x.UpdatedById,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Edges_CreatedById",
+                table: "Edges",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Edges_DeletedById",
+                table: "Edges",
+                column: "DeletedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Edges_FromDestinationId",
+                table: "Edges",
+                column: "FromDestinationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Edges_ToDestinationId",
+                table: "Edges",
+                column: "ToDestinationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Edges_UpdatedById",
+                table: "Edges",
+                column: "UpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_CreatedById",
+                table: "Nodes",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_DeletedById",
+                table: "Nodes",
+                column: "DeletedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_UpdatedById",
+                table: "Nodes",
+                column: "UpdatedById");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Edges");
+
+            migrationBuilder.DropTable(
+                name: "Nodes");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
