9849f70247b1e45f2d24f3ce39a8fdcf0fe524c5
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-07-02 06:07:14 +0600

    new class library created for generic ntier repo and logic layer

---
+++ Sayeed.NTier.Generic/Logic/IBaseService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sayeed.NTier.Generic.Logic
+{
+    public interface IBaseService<T> where T : class
+    {
+        public Task<int> SaveChangesAsync();
+
+        #region basic crud
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<T> FindByIdAsync(long id); // FindAsync() is only for PK's!
+        public Task AddAsync(T item);
+        public void Update(T item);
+        public void UpdateById(long id, T item);
+        public void Delete(T item);
+        public Task DeleteByIdAsync(long id);
+        #endregion
+
+    }
+}
