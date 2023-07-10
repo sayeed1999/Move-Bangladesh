e5a31a2c7d7694c6f243babc331ef57c242d2fd7
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-07-11 04:13:08 +0600

    base class for both rabbitmq publisher subscriber

---
+++ RideSharing.Common/MessageQueues/RabbitMQBase.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+
+namespace RideSharing.Common.MessageQueues
+{
+    public class RabbitMQBase
+    {
+        protected readonly string hostName;
+        protected readonly string exchange;
+        protected readonly string routingKey;
+        protected readonly string exchangeType;
+
+        //TODO:- add topic exchange support
+
+        public RabbitMQBase(string exchange, string? routingKey)
+        {
+            // TODO:- take the hostname from appsettings.json
+            this.hostName = hostName ?? "localhost";
+            this.exchange = exchange ?? string.Empty;
+            this.routingKey = routingKey ?? string.Empty;
+
+            this.exchangeType =
+                this.routingKey == string.Empty
+                ? ExchangeType.Fanout
+                : ExchangeType.Direct;
+
+        }
+    }
+}
