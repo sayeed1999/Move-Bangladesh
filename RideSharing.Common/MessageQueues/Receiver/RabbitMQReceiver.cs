c0ca198cd058002e3c294e9fc8129cf490d16f49
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-07-11 04:14:03 +0600

    class that encapsulates rabbitmq subscribing

---
+++ RideSharing.Common/MessageQueues/Receiver/RabbitMQReceiver.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RideSharing.Common.MessageQueues.Receiver
+{
+    public class RabbitMQReceiver : RabbitMQBase
+    {
+        public RabbitMQReceiver(string exchange) : this(exchange, null) { }
+
+        public RabbitMQReceiver(string exchange, string? routingKey) : base(exchange, routingKey) { }
+
+        public void Start()
+        {
+            var factory = new ConnectionFactory { HostName = hostName };
+
+            Task.Run(() =>
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.ExchangeDeclare(exchange: exchange, type: exchangeType);
+
+                        // declare a server-named queue
+                        var queueName = channel.QueueDeclare().QueueName;
+                        channel.QueueBind(queue: queueName,
+                                          exchange: exchange,
+                                          routingKey: routingKey);
+
+                        var consumer = new EventingBasicConsumer(channel);
+                        consumer.Received += (model, ea) =>
+                        {
+                            byte[] body = ea.Body.ToArray();
+                            var message = Encoding.UTF8.GetString(body);
+                        };
+
+                        channel.BasicConsume(queue: queueName,
+                                             autoAck: true,
+                                             consumer: consumer);
+                    }
+                }
+            });
+        }
+    }
+}
