f017f684ac19be711bf1418b802d4eb1aa85cf20
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-07-11 02:47:00 +0600

    renamed namespace

--- RideSharing.Common/MessageQueues/Messages/IUserModified.cs
+++ RideSharing.Common/MessageQueues/Messages/IUserModified.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RideSharing.Common.MessageBroker.Messages
+namespace RideSharing.Common.MessageQueues.Messages
 {
     public interface IUserModified
     {
