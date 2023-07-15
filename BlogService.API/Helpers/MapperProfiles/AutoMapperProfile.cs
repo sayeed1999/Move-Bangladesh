60a1417e14af4d2434716dc10884fe0665bff7b6
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-07-16 00:58:53 +0600

    setup automapper

---
+++ BlogService.API/Helpers/MapperProfiles/AutoMapperProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using BlogService.Entity;
+using RideSharing.Common.MessageQueues.Messages;
+
+namespace BlogService.API.Helpers.MapperProfiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        private IMapper _mapper;
+
+        public IMapper GetMapperInstance()
+        {
+            if (_mapper == null)
+            {
+                var configuration = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile(this);
+                });
+
+                _mapper = configuration.CreateMapper();
+            }
+
+            return _mapper;
+        }
+
+        public AutoMapperProfile()
+        {
+            CreateMap<UserRegistered, User>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.FirstName + " " + src.LastName));
+
+            CreateMap<UserModified, User>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.FirstName + " " + src.LastName));
+        }
+    }
+}
