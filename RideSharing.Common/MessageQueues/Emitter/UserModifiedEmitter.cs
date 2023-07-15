39788b4acd146e9a401bfbe5568691bc3348ae32
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-07-15 20:35:30 +0600

    create rabbitmq emitter classes for user creation and modify

---
+++ RideSharing.Common/MessageQueues/Emitter/UserModifiedEmitter.cs
@@ -0,0 +1,17 @@
+﻿using RideSharing.Common.MessageQueues.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideSharing.Common.MessageQueues.Emitter
+{
+    public class UserModifiedEmitter : RabbitMQEmitter<UserModified>
+    {
+        public UserModifiedEmitter() : base("user-modified")
+        {
+            
+        }
+    }
+}
---
+++ RideSharing.Common/MessageQueues/Emitter/UserRegisteredEmitter.cs
@@ -0,0 +1,17 @@
+﻿using RideSharing.Common.MessageQueues.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideSharing.Common.MessageQueues.Emitter
+{
+    public class UserRegisteredEmitter : RabbitMQEmitter<UserRegistered>
+    {
+        public UserRegisteredEmitter() : base("user-registered")
+        {
+            
+        }
+    }
+}
