6b3925cffd79b159366e969cdf779a1a03fe2ae1
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-07-22 21:16:25 +0600

    created repository layer for blogservice

---
+++ BlogService.Repository/NodeRepository/NodeRepository.cs
@@ -0,0 +1,20 @@
+﻿using BlogService.Entity;
+using BlogService.Entity.Entities;
+using BlogService.Infrastructure;
+using Sayeed.NTier.Generic.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogService.Service.NodeRepository
+{
+    public class NodeRepository : BaseRepository<Node>, INodeRepository
+    {
+        public NodeRepository(AppDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
