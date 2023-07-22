32335e7cc87ccc136bb435ee23bfb202fe1cb7fe
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-07-22 20:59:52 +0600

    added database migration

--- BlogService.Infrastructure/Migrations/20230722145857_AddUserRelationsTable.Designer.cs
+++ BlogService.Infrastructure/Migrations/20230722145857_AddUserRelationsTable.Designer.cs
@@ -4,6 +4,7 @@ using BlogService.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogService.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230722145857_AddUserRelationsTable")]
+    partial class AddUserRelationsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
