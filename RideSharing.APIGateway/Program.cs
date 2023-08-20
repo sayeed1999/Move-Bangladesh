8af5395c98cf46b6b2185496c303ed97b476fc66
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-08-20 19:56:57 +0600

    sample code added for request transformation through yarp

--- RideSharing.APIGateway/Program.cs
+++ RideSharing.APIGateway/Program.cs
@@ -1,8 +1,19 @@
+using Yarp.ReverseProxy.Transforms;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("yarp"));
+    .LoadFromConfig(builder.Configuration.GetSection("yarp"))
+    .AddTransforms(transforms =>
+    {
+        transforms.AddRequestTransform(transform =>
+        {
+            var requestId = Guid.NewGuid().ToString();
+            transform.ProxyRequest.Headers.Add("x-request-id", requestId);
+            return ValueTask.CompletedTask;
+        });
+    });
 
 var app = builder.Build();
 
