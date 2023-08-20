b27bce684133b02dbd182b5224d283efcb342e70
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-08-20 19:48:13 +0600

    api gateway configured and working

--- RideSharing.APIGateway/Program.cs
+++ RideSharing.APIGateway/Program.cs
@@ -1,28 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services
     .AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("yarp"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
+app.MapGet("/", () => "API Gateway is running.");
 app.MapReverseProxy();
 
 app.Run();
