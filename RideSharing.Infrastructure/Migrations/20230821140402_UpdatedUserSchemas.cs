da82c95e99eb2ca35923725eda313f983fe4da95
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-08-21 20:04:32 +0600

    migration given

---
+++ RideSharing.Infrastructure/Migrations/20230821140402_UpdatedUserSchemas.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RideSharing.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedUserSchemas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AuthUserId",
+                table: "Drivers",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "AuthUserId",
+                table: "Customers",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthUserId",
+                table: "Drivers");
+
+            migrationBuilder.DropColumn(
+                name: "AuthUserId",
+                table: "Customers");
+        }
+    }
+}
