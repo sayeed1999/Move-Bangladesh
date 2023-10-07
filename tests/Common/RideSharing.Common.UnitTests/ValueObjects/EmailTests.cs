357d637818bde52e69ee5212bd875e17eeef5abd
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-10-07 20:39:40 +0600

    tests updated

--- tests/Common/RideSharing.Common.UnitTests/ValueObjects/EmailTests.cs
+++ tests/Common/RideSharing.Common.UnitTests/ValueObjects/EmailTests.cs
@@ -7,34 +7,28 @@ public class EmailTests
     private const string ValidEmail = "abc@gmail.com";
     private const string InvalidEmail = "abc@gmail";
 
-    [SetUp]
-    public void SetUp()
-    {
-        
-    }
-
-    [Test]
+    [Fact]
     public void IsValid_WithValidEmail_ReturnsTrue()
     {
         var result = Email.IsValid(ValidEmail);
-        Assert.IsTrue(result);
+        Assert.True(result);
     }
 
-    [Test]
+    [Fact]
     public void IsValid_WithInvalidEmail_ReturnsFalse()
     {
         var result = Email.IsValid(InvalidEmail);
-        Assert.IsFalse(result);
+        Assert.False(result);
     }
 
-    [Test]
+    [Fact]
     public void Constructor_WithValidEmail_ThrowsException()
     {
         FluentActions.Invoking(() => new Email(ValidEmail))
             .Should().NotThrow<Exception>();
     }
 
-    [Test]
+    [Fact]
     public void Constructor_WithInvalidEmail_ThrowsException()
     {
         FluentActions.Invoking(() => new Email(ValidEmail))
