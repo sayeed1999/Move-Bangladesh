8e88d656a5a34a9d92df96e583194861c41df15c
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-10-07 20:12:23 +0600

    unit test added for domain library

---
+++ tests/Common/RideSharing.Common.UnitTests/ValueObjects/EmailTests.cs
@@ -0,0 +1,44 @@
+﻿using RideSharing.Common.ValueObjects;
+
+namespace RideSharing.Common.UnitTests.ValueObjects;
+
+public class EmailTests
+{
+    private const string ValidEmail = "abc@gmail.com";
+    private const string InvalidEmail = "abc@gmail";
+
+    [SetUp]
+    public void SetUp()
+    {
+        
+    }
+
+    [Test]
+    public void IsValid_WithValidEmail_ReturnsTrue()
+    {
+        var result = Email.IsValid(ValidEmail);
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void IsValid_WithInvalidEmail_ReturnsFalse()
+    {
+        var result = Email.IsValid(InvalidEmail);
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Constructor_WithValidEmail_ThrowsException()
+    {
+        FluentActions.Invoking(() => new Email(ValidEmail))
+            .Should().NotThrow<Exception>();
+    }
+
+    [Test]
+    public void Constructor_WithInvalidEmail_ThrowsException()
+    {
+        FluentActions.Invoking(() => new Email(ValidEmail))
+            .Should().Throw<Exception>();
+    }
+
+}
