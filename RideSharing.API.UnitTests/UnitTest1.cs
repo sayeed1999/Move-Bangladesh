b406e6b5c7407adb7dcb004811a88291070ba87f
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-10-07 19:34:41 +0600

    create new project for unit tests

---
+++ RideSharing.API.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RideSharing.API.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ tests/RideSharing.Entity.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RideSharing.Entity.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
