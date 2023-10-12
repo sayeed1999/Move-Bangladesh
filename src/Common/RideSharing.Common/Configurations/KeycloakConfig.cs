d5f0e41fb18a08df73269d0821fbea41c71f6e7a
msr-wtag <sayeed.rahman@welldev.io>
2023-10-12 19:34:09 +0600

    add keycloak authentication middleware and extensions in common project

---
+++ src/Common/RideSharing.Common/Configurations/KeycloakConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideSharing.Configurations;
+
+public class KeycloakConfig
+{
+    public string AuthUrl { get; set; }
+    public string Tenant { get; set; }
+}
---
+++ src/Common/RideSharing.Common/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace RideSharing.Common.Extensions;
+
+public static class HttpContextExtensions
+{
+    public static bool IsInRole(this HttpContext context, string role)
+    {
+        if (!context.Items.ContainsKey("roles"))
+        {
+            return false;
+        }
+        if (context.Items["roles"] is not List<string> roles)
+        {
+            return false;
+        }
+
+        return roles.Any(r => r == role);
+    }
+
+    public static bool IsAuthenticated(this HttpContext context)
+    {
+        if (!context.Items.ContainsKey("isAuthenticated"))
+        {
+            return false;
+        }
+        if (context.Items["isAuthenticated"] is not bool authenticated)
+        {
+            return false;
+        }
+
+        return authenticated;
+    }
+
+    public static string GetAuthenticationError(this HttpContext context)
+    {
+        if (!context.Items.ContainsKey("Auth.Error"))
+        {
+            return null;
+        }
+        return context.Items["Auth.Error"] as string;
+    }
+
+    public static string GetUserEmail(this HttpContext context)
+    {
+        if (!context.Items.ContainsKey("email"))
+        {
+            return null;
+        }
+        return context.Items["email"] as string;
+    }
+}
