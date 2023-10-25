6a4193d86db653f56e85c3cf62653f85a1df3ba3
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-10-25 23:40:27 +0600

    enhanced appsettings

---
+++ src/Common/RideSharing.Common/Configurations/AppSettings.cs
@@ -0,0 +1,36 @@
+﻿namespace RideSharing.Common.Configurations;
+
+public class AppSettings
+{
+    public ConnectionStrings ConnectionStrings { get; set; }
+    public ClientApplication ClientApplication { get; set; }
+    public RedisServer RedisServer { get; set; }
+    public SmtpServer SmtpServer { get; set; }
+}
+
+public class ConnectionStrings
+{
+    public string DatabaseConnectionString { get; set; }
+}
+
+public class ClientApplication
+{
+    public string AllowedOrigins { get; set; }
+}
+
+public class RedisServer
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Host { get; set; }
+    public string Port { get; set; }
+}
+
+public class SmtpServer
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Host { get; set; }
+    public string Port { get; set; }
+    public string Email { get; set; }
+}
