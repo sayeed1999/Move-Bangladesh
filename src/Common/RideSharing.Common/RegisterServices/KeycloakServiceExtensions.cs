1c19013e27d3b0e1aeb6bc431fc2ce5036b0fa2f
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-10-30 20:36:07 +0600

    breakdown service extensions into classes

---
+++ src/Common/RideSharing.Common/RegisterServices/KeycloakServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
+
+namespace RideSharing.Common.RegisterServices;
+
+public static class KeycloakServiceExtensions
+{
+    public static IServiceCollection ConfigureKeycloakAuthorizationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddKeycloakAuthentication(configuration);
+
+        //services.AddAuthorization(o => o.AddPolicy("IsAdmin", b =>
+        //{
+        //    b.RequireRealmRoles("admin");
+        //    b.RequireResourceRoles("r-admin"); // stands for "resource admin"                                                   // resource roles are mapped to ASP.NET Core Identity roles
+        //    b.RequireRole("r-admin");
+        //}));
+
+        services.AddKeycloakAuthorization(configuration);
+
+        return services;
+    }
+}
