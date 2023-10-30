72047af5e324f62b7c770037f319e4465e243088
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-10-30 21:14:16 +0600

    authorization policies registered in service collection from extension method

--- src/Common/RideSharing.Common/Extensions/KeycloakServiceExtension.cs
+++ src/Common/RideSharing.Common/Extensions/KeycloakServiceExtension.cs
@@ -2,21 +2,24 @@
 using Microsoft.Extensions.DependencyInjection;
 using Keycloak.AuthServices.Authentication;
 using Keycloak.AuthServices.Authorization;
+using RideSharing.Common.Policies;
 
 namespace RideSharing.Common.RegisterServices;
 
 public static class KeycloakServiceExtension
 {
-    public static IServiceCollection ConfigureKeycloakAuthorizationServices(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection ConfigureAuthorizationServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddKeycloakAuthentication(configuration);
 
-        //services.AddAuthorization(o => o.AddPolicy("IsAdmin", b =>
-        //{
-        //    b.RequireRealmRoles("admin");
-        //    b.RequireResourceRoles("r-admin"); // stands for "resource admin"                                                   // resource roles are mapped to ASP.NET Core Identity roles
-        //    b.RequireRole("r-admin");
-        //}));
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(ApplicationPolicy.AdminOnly, AuthorizationPolicies.AdminOnlyPolicy);
+
+            options.AddPolicy(ApplicationPolicy.AdminOrUser, AuthorizationPolicies.AdminOrUserPolicy);
+
+
+        });
 
         services.AddKeycloakAuthorization(configuration);
 
