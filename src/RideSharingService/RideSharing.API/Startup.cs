abfed3cc23726f86c37eeb25627600e851c3b26e
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-10-30 21:13:21 +0600

    method renamed

--- src/RideSharingService/RideSharing.API/Startup.cs
+++ src/RideSharingService/RideSharing.API/Startup.cs
@@ -21,7 +21,7 @@ public static class Startup
             .ConfigureNewtonsoftJson()
             .ConfigureApiBehavior()
             .RegisterSwagger(nameof(API))
-            .ConfigureKeycloakAuthorizationServices(configuration)
+            .ConfigureAuthorizationServices(configuration)
             // register application layers..
             .RegisterInfrastructureLayer()
             .RegisterApplicationLayer();
