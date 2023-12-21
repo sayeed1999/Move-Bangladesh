a1f96933b5d2a336c8a47e4abf62f5e9f002ceca
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-12-21 22:00:05 +0600

    chore: modify test project names and folders

---
+++ tests/RideSharing/RideSharing.Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace RideSharing.Domain.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/RideSharing/RideSharing.InternalAPI.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace RideSharing.InternalAPI.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
