38706f057b9b924bab7d300d8a5cb640bde333bd
sayeed1999 <mdsayeedrahman1999@gmail.com>
2023-12-31 03:15:23 +0600

    feat: add migration to replace string location with spatial data

---
+++ src/RideSharing.Infrastructure/Migrations/20231230205509_UseSpatialDataTypeForLocation.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace RideSharing.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseSpatialDataTypeForLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:postgis", ",,");
+
+            migrationBuilder.AlterColumn<Point>(
+                name: "Source",
+                table: "Trips",
+                type: "geometry (point)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<Point>(
+                name: "Destination",
+                table: "Trips",
+                type: "geometry (point)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:PostgresExtension:postgis", ",,");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Source",
+                table: "Trips",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(Point),
+                oldType: "geometry (point)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Destination",
+                table: "Trips",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(Point),
+                oldType: "geometry (point)");
+        }
+    }
+}
