e56237918aa98480b6e3d485d2bdaef56b644af3
sayeed1999 <mdsayeedrahman1999@gmail.com>
2024-01-06 14:55:54 +0600

    feat: create minimal api project with net8 lts

--- src/RideSharing.CustomerAPI/Program.cs
+++ src/RideSharing.CustomerAPI/Program.cs
@@ -4,36 +4,36 @@ namespace RideSharing.CustomerAPI;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
 
-        // Register prefixed only environment variables.
-        builder.Configuration.AddEnvironmentVariables("API__");
+		// Register prefixed only environment variables.
+		builder.Configuration.AddEnvironmentVariables("API__");
 
-        builder.Services.ConfigureServices(builder.Configuration, builder.Environment);
+		builder.Services.ConfigureServices(builder.Configuration, builder.Environment);
 
-        var app = builder.Build();
+		var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
 
-        // Custom middlewares.
-        app.UseMiddleware<ExceptionHandlingMiddleware>();
-        app.UseMiddleware<CustomExceptionHandlingMiddleware>();
+		// Custom middlewares.
+		app.UseMiddleware<ExceptionHandlingMiddleware>();
+		app.UseMiddleware<CustomExceptionHandlingMiddleware>();
 
-        app.UseHttpsRedirection();
-        app.UseAuthentication();
-        app.UseAuthorization();
+		app.UseHttpsRedirection();
+		app.UseAuthentication();
+		app.UseAuthorization();
 
-        app.MapControllers();
+		app.MapControllers();
 
-        app.MapGet("/", () => "RideSharing.InternalAPI is running.");
+		app.MapGet("/", () => "RideSharing.CustomerAPI is running.");
 
-        app.Run();
-    }
+		app.Run();
+	}
 }
