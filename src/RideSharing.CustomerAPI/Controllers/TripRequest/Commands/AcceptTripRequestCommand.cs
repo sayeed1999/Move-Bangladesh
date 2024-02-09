56c2e161b23b32b8816772a1fde783e5a477bb96
sayeed1999 <mdsayeedrahman1999@gmail.com>
2024-02-09 21:35:37 +0600

    feat: create endpoint for accept trip

---
+++ src/RideSharing.CustomerAPI/Controllers/TripRequest/Commands/AcceptTripRequestCommand.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RideSharing.Application.TripRequestUseCase.Commands.AcceptTripRequestCommand;
+using RideSharing.Common.Entities;
+
+namespace RideSharing.CustomerAPI.Controllers.TripRequest.Commands
+{
+	[Route("api/external/trip-requests")]
+	[ApiController]
+	public class AcceptTripRequestCommand : ControllerBase
+	{
+		private readonly IMediator mediator;
+
+		public AcceptTripRequestCommand(IMediator mediator)
+		{
+			this.mediator = mediator;
+		}
+
+		/// <summary>
+		/// Use this endpoint to accept trip request for driver
+		/// </summary>
+		/// <param name="tripRequestId"></param>
+		/// <returns></returns>
+		[HttpPut("{tripRequestId}")]
+		public async Task<ActionResult<Response<AcceptTripRequestResponseDto>>> RequestRide(Guid tripRequestId)
+		{
+			var driverId = new Guid(); // TODO:- get customerId from httpContextAccessor!
+
+			var model = new AcceptTripRequestDto(driverId, tripRequestId);
+
+			var res = await mediator.Send(model);
+
+			if (res.IsFailure) return BadRequest(res.Error);
+
+			return Ok(res.Value);
+		}
+	}
+}
