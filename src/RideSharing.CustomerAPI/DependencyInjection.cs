33a11213a2255fc2664d428708502b3a9c619fbc
sayeed1999 <mdsayeedrahman1999@gmail.com>
2024-05-30 01:26:24 +0600

    chore: remove unused DI

--- src/RideSharing.CustomerAPI/DependencyInjection.cs
+++ src/RideSharing.CustomerAPI/DependencyInjection.cs
@@ -20,16 +20,6 @@ public static class DependencyInjection
 	public static IServiceCollection RegisterDatabaseRepositories(this IServiceCollection services)
 		=> services
 			.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>))
-			//.AddScoped<ICabRepository, CabRepository>()
-			//.AddScoped<ICustomerRatingRepository, CustomerRatingRepository>()
-			//.AddScoped<ICustomerRepository, CustomerRepository>()
-			//.AddScoped<IDriverRatingRepository, DriverRatingRepository>()
-			//.AddScoped<IDriverRepository, DriverRepository>()
-			//.AddScoped<IPaymentRepository, PaymentRepository>()
-			//.AddScoped<ITripRequestRepository, TripRequestRepository>()
-			//.AddScoped<ITripRequestLogRepository, TripRequestLogRepository>()
-			//.AddScoped<ITripRepository, TripRepository>()
-			//.AddScoped<ITripLogRepository, TripLogRepository>()
 			.AddScoped<IUnitOfWork, UnitOfWork>();
 
 	public static IServiceCollection RegisterEventBuses(this IServiceCollection services)
