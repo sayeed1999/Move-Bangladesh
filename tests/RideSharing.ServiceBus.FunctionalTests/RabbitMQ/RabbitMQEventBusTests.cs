6673b1000ad9f2e96b7b3ef73baeade65d52be51
sayeed1999 <mdsayeedrahman1999@gmail.com>
2024-06-29 18:23:55 +0600

    chore: add dependencies and add sample test

---
+++ tests/RideSharing.ServiceBus.FunctionalTests/RabbitMQ/RabbitMQEventBusTests.cs
@@ -0,0 +1,13 @@
+﻿namespace RideSharing.ServiceBus.FunctionalTests.RabbitMQ;
+
+public class RabbitMQEventBusTests
+{
+	[Fact]
+	public void SampleTest_One_ShouldEqual_One()
+	{
+		var x = 1;
+		x.Should().Be(1);
+	}
+
+
+}
